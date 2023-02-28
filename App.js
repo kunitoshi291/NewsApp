3d39ba45c3e7ff27b685f2fcf29146042f46dc95
Takekuni <takevadogu@outlook.jp>
2023-02-28 23:08:31 +0900

    style compornent & image compornemt

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, View, Text } from 'react-native';
 
 export default function App() {
+  const sampleText = " I love Nature.I love Nature.I love Nature.I love Nature.I love Nature.I love Nature.I love Nature.I love Nature."
   return (
     <View style={styles.container}>
-      <Text>Hello React Native</Text>
+      <View style={styles.itemContainer}>
+        <View style={styles.leftContainer}>
+          <Image 
+            style={{ width: 100, height: 100 }}
+            source={{ uri: "https://picsum.photos/id/10/300/300" }}
+          />
+        </View>
+        <View style={styles.rightContainer}>
+          <Text numberOfLines={2} style={styles.Text}>
+            {sampleText}
+          </Text>
+          <Text style={styles.subText}>ReactNews</Text>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +27,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eee',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  itemContainer: {
+    height: 100,
+    width: "100%",
+    flexDirection: "row",
+    backgroundColor: "white",
+  },
+  leftContainer: {
+    width:100,
+  },
+  rightContainer: {
+    flex: 1,
+    padding: 10,
+    justifyContent: "space-between",
+  }, 
+  text: {
+    fontSize: 16,
+  },
+  subText: {
+    fontSize: 12,
+    color: "gray"
+  }
+
 });
