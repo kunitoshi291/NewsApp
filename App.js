842ecf825bf5ab18576818253b3b30cfe7dacc46
Takekuni <takevadogu@outlook.jp>
2023-03-11 14:51:12 +0900

    ConstantsにAPIキーを読み込む処理を実装

--- App.js
+++ App.js
@@ -1,9 +1,29 @@
+import {useState, useEffect} from "react"
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, SafeAreaView, FlatList } from 'react-native';
 import {ListItem}from './components/ListItem';
-import articles from './dummies/articles';
+import axios from "axios";
+import Constants from "expo-constants";
 
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${Constants.manifest.extra.newsApiKey}`;
 export default function App() {
+  const [articles, setArticles] = useState([]);
+
+  const fetchArticles = async() => {
+    try {
+      const response = await axios.get(URL)
+      console.log(response.data.articles);
+      setArticles(response.data.articles)
+
+    }catch(error){
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchArticles()
+  }, [])
+
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
