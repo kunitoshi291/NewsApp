a618f78d12ccfbb378c2600bb4456bfcd747cec6
Takekuni <takevadogu@outlook.jp>
2023-03-22 22:59:54 +0900

    タップしたら、ニュースの詳細画面が表示される処理を実装

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -33,7 +33,7 @@ export const HomeScreen = ({navigation}) => {
           imageUrl = {item.urlToImage}
           title = {item.title}
           author = {item.author}
-          onPress= {() => navigation.navigate("Article") } />
+          onPress= {() => navigation.navigate( "Article", {article: item } ) } />
   )}
       keyExtractor={(item, index) => index.toString()}
       />
