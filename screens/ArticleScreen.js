2606439344d4bf61d937c1dfb8e647bb8540835a
Takekuni <takevadogu@outlook.jp>
2023-07-15 19:38:15 +0900

    actionをdispathして、storeの情報を表示させる

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,14 +1,26 @@
 import { StyleSheet, SafeAreaView, Text } from "react-native";
 import { WebView } from 'react-native-webview';
+import {ClipButton} from "../components/ClipButton";
+import {useDispatch, useSelector} from "react-redux";
+import {addClip, deleteClip} from "../store/userSlice"
 
 export const ArticleScreen = ({route}) => {
     const {article} = route.params;
+    const dispatch = useDispatch();
+    const clips = useSelector((state) => state.user.clips);
+    const isClipped = clips.some((clip) => clip.url === article.url);
+
+    const onPressClip = () => {
+        if(isClipped) {
+            dispatch(deleteClip(article));
+        }else {
+            dispatch(addClip(article));
+        }
+    }
     return (
         <SafeAreaView style={styles.container}>
-            <WebView
-             style={styles.container}
-             source={{ uri: article.url }}
-            />
+            <ClipButton onPress={onPressClip} enabled={isClipped} />
+            <WebView source={{ uri: article.url }} />
         </SafeAreaView>
     )
 };
