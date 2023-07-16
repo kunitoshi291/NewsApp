94a502807db0e298ad4736451b984c2cf2932c4e
Takekuni <takevadogu@outlook.jp>
2023-07-16 19:44:51 +0900

    reduxの状態管理の永続化

--- store/index.js
+++ store/index.js
@@ -1,8 +1,22 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from 'redux-persist'
 import userReducer from "./userSlice";
+import { combineReducers } from 'redux';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { configureStore } from '@reduxjs/toolkit';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const rootReducer = combineReducers({
+    user: userReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: {
-        user: userReducer,
-    },
-})
+    reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
