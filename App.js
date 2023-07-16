a874df5eb8fabed3daf2cea7e98722aff91753fc
Takekuni <takevadogu@outlook.jp>
2023-07-16 19:44:05 +0900

    redux-persistでreduxの状態管理の永続化

--- App.js
+++ App.js
@@ -5,8 +5,9 @@ import {HomeScreen} from './screens/HomeScreen';
 import {ArticleScreen} from './screens/ArticleScreen';
 import {ClipScreen} from './screens/ClipScreen';
 import {FontAwesome} from '@expo/vector-icons'
-import { store } from "./store";
+import { store, persistor } from "./store";
 import {Provider} from "react-redux"
+import { PersistGate } from "redux-persist/integration/react";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -24,21 +25,54 @@ const screenOption = ({ route }) => ({
 const HomeStack = () => {
   return(
     <Stack.Navigator>
-    <Stack.Screen name="HomeTab" component={HomeScreen} options={{headerShown: false}}/>
-    <Stack.Screen name="Article" component={ArticleScreen} />
+    <Stack.Screen
+      name="HomeTab"
+      component={HomeScreen}
+      options={{headerShown: false}}
+    />
+    <Stack.Screen
+      name="Article"
+      component={ArticleScreen}
+    />
   </Stack.Navigator>
   )
 };
 
+const ClipStack = () => {
+  return(
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Clip"
+        component={ClipScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Article"
+        component={ArticleScreen}
+      />
+  </Stack.Navigator>
+  );
+}
+
 export default function App() {
   return (
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
     <NavigationContainer>
       <Tab.Navigator screenOptions={screenOption}>
-        <Tab.Screen name="HomeTab" component={HomeStack} options={{headerShown: false, title: "Home"}} />
-        <Tab.Screen name="ClipTab" component={ClipScreen} options={{headerShown: false, title: "Clip"}} />
+        <Tab.Screen
+          name="HomeTab"
+          component={HomeStack}
+          options={{headerShown: false, title: "Home"}}
+        />
+        <Tab.Screen
+          name="ClipTab"
+          component={ClipStack}
+          options={{headerShown: false, title: "Clip"}}
+        />
       </Tab.Navigator>
     </NavigationContainer>
+    </PersistGate>
     </Provider>
   );
 };
